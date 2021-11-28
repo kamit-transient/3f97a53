b25e4dd1c78c6867ee819488c32c678b7b931609
kamit-transient <hey.amit@outlook.com>
2021-11-28 16:49:31 +0000

    added react-spring to animate

--- src/components/pages/excel/excelPage.js
+++ src/components/pages/excel/excelPage.js
@@ -2,22 +2,54 @@ import React, { useContext, useEffect } from 'react';
 import Link from 'next/link';
 import AppContext from '../../../context/appContext';
 import FileRenderer from "../../fileRenderer";
+import Converters from '../../../data/converters';
+import {useSpring, animated,config } from "@react-spring/web"
+
 
 export default function ExcelPage() {
 	let { appState, setAppState } = useContext(AppContext);
+	let animatedStyle= useSpring({
+		from:{transform:"translateY(-1200px) scale(0)"},
+		to:{transform:"translateY(0px) scale(1)"},
+		 delay: 200,
+    config: config.molasses,
+	})
+	
+	let prefix=appState.prefix;
+	
+	let formatsToBeConverted= Converters["excel"].children.filter(ch=>ch.implemented);
 
 	useEffect(() => {}, []);
+	
+	let file={
+		name:"ammmm",
+		size:121212
+	}
 
 	return (
-		<div className="container mx-auto">
-			<div className="flex flex-col gap-10">
+		<div className="container mx-auto py-14">
+			<div className="flex flex-col items-center justify-center gap-10">
 				<div>
-					{appState.files.map((file) => {
+					{appState.files && appState.files.map((file) => {
 						return <FileRenderer file={file} key={file.name} />;
 					})}
+					<FileRenderer file={file} key={file.name} />
 				</div>
 				
-			</div>{' '}
+				<animated.div style={animatedStyle} className="flex flex-wrap gap-2 items-center justify-start">
+				{
+					formatsToBeConverted &&	formatsToBeConverted.map(f=>{
+							return(
+									<div key={f.prefix} className="bg-primary text-white font-heading tracking-wide cursor-pointer px-6 py-3  rounded-md uppercase text-3xl ">
+										{f.name}
+									</div>
+							)
+						})
+					}
+				</animated.div>
+				
+			</div>
+			
 		</div>
 	);
 }
